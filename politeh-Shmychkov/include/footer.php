24f8bb77dc83dde7b092ba5e919777a150c3cfa0
Xonda1234342 <125695686+Xonda1234342@users.noreply.github.com>
2023-02-28 18:12:41 +0300

    Add files via upload

---
+++ politeh-Shmychkov/include/footer.php
@@ -0,0 +1,69 @@
+Б<html>
+    <body>
+    <div class = "footer">
+          <div>
+              <div class = "sber"><img src = "./assets/img/sberbank_icon.png" class = "sb" width="35px" height="35px"><div class = "text-sber">Сибирь банк</div></div>
+              <div class = "tele">8 800 999 55 55</div>
+              <div class = "phone">Звонок по россии бесплатно</div>
+          </div>
+          <div>
+              <div class = "text-footer">
+                  Банк
+              </div>
+              <ul>
+                  <li>О банке</li>
+                  <li>Новости</li>
+                  <li>Курс валют</li>
+                  <li>Вакансии</li>
+                  <li>Банкоматы</li>
+                  <li>Контакты</li>
+              </ul>
+          </div>
+          <div>
+              <div class = "text-footer">
+                  Карты
+              </div>
+              <ul>
+                  <li>Кредитная</li>
+                  <li>Депитовная</li>
+                  <li>Зарплатная</li>
+                  <li>Пенсионная</li>
+                  <li>Социальная</li>
+                  <li>Молодежная</li>
+              </ul>
+          </div>
+          <div>
+              <div class = "text-footer">
+                  Карты
+              </div>
+              <ul>
+                  <li>Инвенстиции</li>
+                  <li>Брокарский счет</li>
+                  <li>Облигации</li>
+                  <li>Паевые фонды</li>
+                  <li>Управление</li>
+                  <li>Трейдинг</li>
+                  <li>Обучение</li>
+              </ul>
+          </div>
+          <div>
+              <div class = "text-footer">
+                  Ипотека
+              </div>
+              <ul>
+                  <li>Новостройки</li>
+                  <li>Вторичное жилье</li>
+                  <li>Рефинансирование</li>
+                  <li>Для госслужащих</li>
+                  <li>Гос поддержка</li>
+                  <li>Ипотечный калькулятор</li>
+              </ul>
+          </div>
+      </div>
+      <hr>
+      <div class = "footer-down">
+          <div>2020 @ПАО Сибирь банк</div>
+          <div>Политика конфиденциальности</div>
+      </div>
+  </body>
+</html>
---
+++ politeh-Shmychkov/include/header.php
@@ -0,0 +1,75 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./assets/css/sait.css" />
+    <link rel="stylesheet" href="./assets/css/rar.css" />
+    <title>Практическая работа 6</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="header">
+        <div class="menu-wrapper">
+          <div class="logo">
+            <img src="./assets/img/logo.png" alt="logo" />
+          </div>
+
+          <div class="menu">
+            <span>О нас</span>
+            <span class="active">Преимущества</span>
+            <span>Цены</span>
+            <span>Акции</span>
+            <span>Отзывы</span>
+            <span>Контакты</span>
+          </div>
+        </div>
+
+        <div class="actions">
+          <button class="login">Вход</button>
+          <button class="signup">Регистрация</button>
+        </div>
+      </div>
+    </div>
+      
+      <div class="content">
+        <div class="menu-style">
+          <ol>Завтраки</ol>
+          <ol>Первые блюда</ol>
+          <ol>Вторые блюда</ol>
+          <ol>Десерты</ol>
+          <ol>Выпечка</ol>
+          <ol>Соусы</ol>
+          <ol>Напитки</ol>
+          <ol>Закуски</ol> 
+          <ol>Заготовки</ol><hr>
+          <ol>О посуде</ol></hr>
+          <ol>Лайфхаки</ol>
+          <ol>Статьи</ol> 
+          <hr></hr>        
+          <img src="./assets/img/Vk_icon.png" alt="Vk_icon"/> 
+          <img src="./assets/img/in_icon.png" alt="in_icon"/>
+          <img src="./assets/img/fb_icon.png" alt="fb_icon"/>     
+        </div>       
+            <div class="header1">                   
+              <p class="Best">Best Resipes</p>
+              <p class="recept">Рецепт на любой случай</p>
+              <hr>
+              <div class = "lol">                 
+                <div class="Linia">                 
+                  <img src="./assets/img/tost.jpg" alt="tost"  width= "350" />             
+                </div>                  
+                  <div class="TextOcnv">
+                    <p id="a1">Главная/Завтраки/Сырники</p>
+                    <div>
+                    <p id="a2"> Твороженные с сырниками и бананом и шоколадом. </p>
+                    </div> 
+                  <div>                  
+                    <p id="a3">Необычный варианта десерта, который особенно оценят сладкоежки<br> - сырники с бананом и шоколадом.</p>
+                  </div>
+                </div> 
+              </div>
+            </div>               
+        </div>
+  </body>
+</html>
